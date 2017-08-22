091cdd34b96a21ecc6129d378ba7be30922be73a
Evans Musomi <evans.musomi@gmail.com>
2017-08-22 16:27:39 +0300

    Adds eslint and fixes eslint errors

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
+import express from 'express';
 import config from './config';
 import apiRouter from './api';
 
-import express from 'express';
 const server = express();
 
 server.set('view engine', 'ejs');
 
 server.get('/', (req, res) => {
-    res.render('index', {
-        content: 'Hello Express and <b>EJS</b>'
-    });
+  res.render('index', {
+    content: 'Hello Express and <b>EJS</b>'
+  });
 });
 
 server.use('/api', apiRouter);
 server.use(express.static('public'));
 
 server.listen(config.port, () => {
-    console.info('Express listening on port ', config.port);
+  console.info('Express listening on port ', config.port);
 });
