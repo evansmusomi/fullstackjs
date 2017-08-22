520834ff4845be30df0615b2d8b779ff25226210
Evans Musomi <evans.musomi@gmail.com>
2017-08-22 17:30:38 +0300

    Creates App and Header components as modules

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './Header';
+
+const App = () => (
+  <div className="App">
+    <Header message='Naming Contests' />
+    <div>
+      ...
+    </div>
+  </div>
+);
+
+export default App;
