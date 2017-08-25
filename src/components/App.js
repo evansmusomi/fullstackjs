5a9b3220a16bcbc346ca20389020625c1687a160
Evans Musomi <evans.musomi@gmail.com>
2017-08-25 10:54:46 +0300

    React client fetches data from a remote API

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import Header from './Header';
 import ContestPreview from './ContestPreview';
 
@@ -7,8 +8,23 @@ import ContestPreview from './ContestPreview';
  */
 class App extends React.Component {
   state = { 
-    pageHeader: 'Naming Contests'
+    pageHeader: 'Naming Contests',
+    contests: []
   };
+
+  /**
+   * Update mounted component
+   * @returns {state} updated state
+   */
+  componentDidMount() {
+    axios.get('/api/contests').then(
+      (resp) => {
+        this.setState({ 
+          contests: resp.data.contests 
+        })
+      }
+    ).catch(console.error);
+  }
 
   /**
    * Render app
@@ -19,8 +35,8 @@ class App extends React.Component {
       <div className="App">
         <Header message={ this.state.pageHeader} />
         <div>
-          { this.props.contests.map(contest => 
-          <ContestPreview { ...contest } />
+          { this.state.contests.map(contest => 
+          <ContestPreview key={ contest.id } { ...contest } />
           ) }
         </div>
       </div>
