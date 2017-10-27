31a4a3d330d75fa94b543f8ddfeb0e272bf7130d
Evans Musomi <evans.musomi@gmail.com>
2017-10-27 10:15:24 +0300

    Renders initial content from the server

--- config.js
+++ config.js
@@ -3,5 +3,10 @@ const env = process.env;
 export const nodeEnv = env.nodeEnv || 'development'
 
 export default {
-    port: env.port || 8080
+    port: env.port || 8080,
+    host: env.HOST || '0.0.0.0',
+    get serverUrl(){
+        return `http://${this.host}:${this.port}`;
+    }
+
 };
--- server.js
+++ server.js
@@ -13,15 +13,21 @@ server.use(sassMiddleware({
 
 server.set('view engine', 'ejs');
 
+import serverRender from './serverRender';
+
 server.get('/', (req, res) => {
-  res.render('index', {
-    content: 'Hello Express and <b>EJS</b>'
-  });
+  serverRender()
+    .then(content => {
+      res.render('index', {
+        content
+      });
+    })
+    .catch(console.error);
 });
 
 server.use('/api', apiRouter);
 server.use(express.static('public'));
 
-server.listen(config.port, () => {
+server.listen(config.port, config.host, () => {
   console.info('Express listening on port ', config.port);
 });
